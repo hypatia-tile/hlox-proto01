026a444ae9c4be40666a8176691b9386451eb48a
shinokun <hypatia.tile02021@gmail.com>
2026-02-27 00:05:35 +0900

    Read command line argument

--- hlox/app/Main.hs
+++ hlox/app/Main.hs
@@ -1,4 +1,28 @@
 module Main (main) where
 
+import System.Directory (getCurrentDirectory)
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import System.IO (hFlush, stdout)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = greet'
+
+greet' :: IO ()
+greet' = do
+  args <- getArgs
+  case args of
+    [] -> putStrLn "No input file provided."
+    [input] -> putStrLn $ "Input file: " <> input
+    _ -> do
+      putStrLn "Usage: hlox [input_file]"
+      exitWith (ExitFailure 64)
+
+greet :: IO ()
+greet = do
+  cwd <- getCurrentDirectory
+  putStrLn $ "Current directory: " <> cwd
+  putStr "Input file: "
+  hFlush stdout
+  input <- getLine
+  putStrLn input
