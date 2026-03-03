97455c6b580775ecf1a73e1b392e987257f3d11c
shinokun <hypatia.tile02021@gmail.com>
2026-03-04 00:15:56 +0900

    adjust one or to character tokens

--- hlox/src/Interp/Lex/Lexer.hs
+++ hlox/src/Interp/Lex/Lexer.hs
@@ -4,6 +4,8 @@ import Control.Monad
 import Control.Monad.State.Lazy
 import Interp.Data.Token
 
+type LexerVal = Token
+
 data TokenWithPosition = TokenWithPosition
   { token :: Token,
     tokenStart :: Position,
@@ -25,12 +27,23 @@ data LexerState = LexerState
 test :: IO ()
 test = do
   print $ parseS "Hello"
+  print $ parseS ";Hello"
+  print $ parseS ",Hello"
   print $ parseS ":Hello"
-  print $ parseS "((Hello"
+  print $ parseS ".Hello"
+  print $ parseS "(Hello"
   print $ parseS ")(Hello"
+  print $ parseS "!Hello"
+  print $ parseS "!=Hello"
+  print $ parseS "=Hello"
+  print $ parseS "==Hello"
+  print $ parseS ">Hello"
+  print $ parseS ">=Hello"
+  print $ parseS "<Hello"
+  print $ parseS "<=Hello"
 
-parseS :: String -> Maybe (Token, LexerState)
-parseS src = singleCharToken' . newLexerState $ src
+parseS :: String -> Maybe (LexerVal, LexerState)
+parseS src = singleOrDoubleCharToken . newLexerState $ src
 
 newLexerState :: String -> LexerState
 newLexerState src = LexerState src (Position 0 0)
@@ -41,28 +54,43 @@ addCol n p = p {column = column p + n}
 addLine :: Int -> Position -> Position
 addLine n p = p {line = line p + n}
 
-singleCharToken' :: LexerState -> Maybe (Token, LexerState)
-singleCharToken' source = do
-  (c, rest) <- getC source
-  tok <- singleCharToken c
-  return (tok, rest {pos = addCol 1 (pos rest)})
+singleOrDoubleCharToken :: LexerState -> Maybe (LexerVal, LexerState)
+singleOrDoubleCharToken lexerState = do
+  (x,y) <- getC lexerState
+  matchTok x y
+  where
+    matchTok :: Char -> LexerState -> Maybe (LexerVal, LexerState)
+    matchTok '!' = Just . weighTok TokBang TokBangEqual ('=' ==)
+    matchTok '=' = Just . weighTok TokEqual TokEqualEqual ('=' ==)
+    matchTok '>' = Just . weighTok TokGreater TokGreaterEqual ('=' ==)
+    matchTok '<' = Just . weighTok TokLess TokLessEqual ('=' ==)
+    matchTok _ = \_ -> singleCharToken lexerState
+    weighTok :: Token -> Token -> (Char -> Bool) -> LexerState -> (LexerVal, LexerState)
+    weighTok tok1 tok2 pred st = case getC st of
+      Nothing -> (tok1, st { pos = addCol 1 (pos st) })
+      Just (c, r) -> 
+        if pred c
+        then (tok2, r { pos = addCol 2 (pos r) })
+        else (tok1, st { pos = addCol 1 (pos st) })
 
-singleCharToken :: Char -> Maybe Token
-singleCharToken '(' = Just TokLeftParen
-singleCharToken ')' = Just TokRightParen
-singleCharToken '{' = Just TokLeftBrace
-singleCharToken '}' = Just TokRightBrace
-singleCharToken ',' = Just TokComma
-singleCharToken '.' = Just TokDot
-singleCharToken '-' = Just TokMinus
-singleCharToken '+' = Just TokPlus
-singleCharToken ';' = Just TokSemicolon
-singleCharToken '*' = Just TokStar
-singleCharToken '!' = Just TokBang
-singleCharToken '=' = Just TokEqual
-singleCharToken '>' = Just TokGreater
-singleCharToken '<' = Just TokLess
-singleCharToken _ = Nothing
+singleCharToken :: LexerState -> Maybe (LexerVal, LexerState)
+singleCharToken source = do
+  (c, rest) <- getC source
+  tok <- matchTok c
+  return (tok, rest {pos = addCol 1 (pos rest)})
+  where
+    matchTok :: Char -> Maybe Token
+    matchTok '(' = Just TokLeftParen
+    matchTok ')' = Just TokRightParen
+    matchTok '{' = Just TokLeftBrace
+    matchTok '}' = Just TokRightBrace
+    matchTok ',' = Just TokComma
+    matchTok '.' = Just TokDot
+    matchTok '-' = Just TokMinus
+    matchTok '+' = Just TokPlus
+    matchTok ';' = Just TokSemicolon
+    matchTok '*' = Just TokStar
+    matchTok _ = Nothing
 
 getC :: LexerState -> Maybe (Char, LexerState)
 getC state =
